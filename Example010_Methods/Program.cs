1f38f79c0b468fea6664a655c060eb67c0887915
Asya <shulga-a.91@live.ru>
2023-12-08 22:43:31 +0700

    Example011_ArrayLibrary

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {31, 72, 39, 81, 85, 66, 27, 81};
+
+int n = array.Length;
+int find = 81;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
